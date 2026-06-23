a2e414f3c3bab67632d7ce47d043029eb6b67580
rojojun <zoloman316@gmail.com>
2026-06-23 11:24:38 +0900

    feat: App에 getGreeting 메서드 골격 추가

--- app/src/main/java/com/system/batch/App.java
+++ app/src/main/java/com/system/batch/App.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
-    public static void main(String[] args) {
-        System.exit(SpringApplication.exit(SpringApplication.run(App.class, args)));
-    }
+  public static void main(String[] args) {
+    System.exit(SpringApplication.exit(SpringApplication.run(App.class, args)));
+  }
+
+  public Object getGreeting() {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'getGreeting'");
+  }
 }
